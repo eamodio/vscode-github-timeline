241947daa29fa19b177e8c6ef82bc064cccf3b2b
Eric Amodio <eamodio@gmail.com>
2021-02-19 12:57:17 -0500

    Adds basic auth scaffolding

--- src/extension.ts
+++ src/extension.ts
@@ -1,14 +1,27 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { graphql } from '@octokit/graphql';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export async function activate(context: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "github-timeline" is now active!');
+
+	let session;
+	try {
+		session = await vscode.authentication.getSession('github', ['repo'], { createIfNone: true });
+	} catch (ex) {
+		// TODO: User didn't sign in/give access
+		return;
+	}
+
+	const api = graphql.defaults({ headers: { authorization: `Bearer ${session.accessToken}` } });
+	// Can make calls by providing a query and args
+	// api(query, {});
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
